48ec79d40041c26a1d96952f65a22c61be558961
qiangliu.7@outlook.com <qiangliu.7@outlook.com>
2025-10-07 15:45:13 +0200

    add missing plot_field_slice in __init__

--- torchfsm/plot/app/__init__.py
+++ torchfsm/plot/app/__init__.py
@@ -1,5 +1,5 @@
 from .traj_field import plot_traj, plot_field
 from .frame import plot_traj_frame
-from .slice import plot_traj_slice
+from .slice import plot_traj_slice, plot_field_slice
 from .frame_slice import plot_traj_frame_slice
 from .error_compare import *
