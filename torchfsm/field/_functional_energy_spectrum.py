ca456c3626c61239fc76e77295795661d510b198
qiangliu.7@outlook.com <qiangliu.7@outlook.com>
2025-12-01 13:19:33 +0100

    fix the import name for `functional_energy_spectrum`

--- torchfsm/field/_functional_energy_spectrum.py
+++ torchfsm/field/_functional_energy_spectrum.py
@@ -29,7 +29,7 @@ def functional_energy_spectrum(
     n_batch: int,
     n_channels: int,
     spectrum_func: Callable[[torch.Tensor], torch.Tensor],
-    normalize_mode: Optional[Literal["normal_distribution", "-1_1", "0_1"]] = None,
+    normalize_mode: Optional[Literal["normal_distribution","-1_1","0_1","min_max"]] = None,
 ) -> SpatialTensor["B C H ..."]:
     """
     Generate an field $\mathbf{u}$ based on a given energy spectrum function $E(k)$ which statisfies$\frac{1}{2}\oiint_{A(K)}\hat{\mathbf{u}}(\mathbf{k})\hat{\mathbf{u}}^*(\mathbf{k})dA(k)=E(k)$
@@ -44,7 +44,7 @@ def functional_energy_spectrum(
         n_batch (int): The number of batches.
         n_channels (int): The number of channels. Note that if multiple channels are used, each channel will be treated as a component of the vector field ans the energy is equally distributed among all channels.
         spectrum_func (Callable[[torch.Tensor], torch.Tensor]): A function that takes a tensor of wave numbers and returns the corresponding energy spectrum values, e.g., lambda k: 0.327*k**(-5/3).
-        normalize_mode (Optional[Literal["normal_distribution","-1_1","0_1"]]): The normalization mode for the generated noise.
+        normalize_mode (Optional[Literal["normal_distribution","-1_1","0_1","min_max"]]): The normalization mode for the generated noise.
             If None, no normalization is applied. Default is None.
             Note that normalization will **change** the energy spectrum of the generated field.
 
